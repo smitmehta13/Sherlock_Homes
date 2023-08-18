8fa172d4a53511a1b4833d00d5188dade75dd2f1
Smit Mehta <smitmehta.work@gmail.com>
2023-08-18 04:51:01 -0400

    normal navbar

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-   return render(request, 'index.html')
+   return render(request, 'index.html', {'title': 'Home'})
+
+def about(request):
+   return render(request, 'about.html', {'title': 'About'})
